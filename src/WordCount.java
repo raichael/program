dc03205a6f477836168a80edd7346117df6e156e
raichael <raichujas@gmail.com>
2019-06-07 17:48:31 +0530

    character occurrence

---
+++ src/WordCount.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class WordCount {
+    public static void main(String[] args)
+    {
+        int count=0;
+        System.out.println("Enter the string");
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        for (char i='a';i<='z';i++)
+        {
+            for(int j=0;j<s.length();j++)
+            {
+                if(s.charAt(j)==i)
+                {
+                    count++;
+                }
+            }
+            if(count!=0)
+            {
+                System.out.println(i+"="+count);
+                count =0;
+            }
+        }
+    }
+}
